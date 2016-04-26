60a86a96132753a1cd1487fb3ea4864d2a092910
Murali <itsme2murali@gmail.com>
2016-04-26 13:23:20 +0530

    Changes

---
+++ src/main/java/com/javacodegeeks/enterprise/rest/resteasy/MainTest.java
@@ -0,0 +1,13 @@
+package com.javacodegeeks.enterprise.rest.resteasy;
+
+public class MainTest extends TestInterface1{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StaticTestClass test=new StaticTestClass("Murali");
+		System.out.println(test.getMethod());
+		
+	}
+
+	
+}
---
+++ src/main/java/com/javacodegeeks/enterprise/rest/resteasy/TestInterface1.java
@@ -0,0 +1,5 @@
+package com.javacodegeeks.enterprise.rest.resteasy;
+
+public class TestInterface1 {
+int i=10;
+}
